455bd1427c07f8c4753aa5329257df650aa27d16
ArtVVB <arthur.brown@digilent.com>
2022-06-24 13:55:39 -0700

    Add initial stab at xdc generation
    Copies the ZmodScopeController's approach of just false-pathing everything in the handshake module.

---
+++ write_xdc.tcl
@@ -0,0 +1,31 @@
+set script_dir [file dirname [file normalize [info script]]]
+source ${script_dir}/util.tcl
+
+set outfile_path ${script_dir}/out/cdc.xdc
+set specfile_path ${script_dir}/tpl/example.json
+
+#script has no calling context, so set up data for testing
+package require json
+source [file join $script_dir util.tcl]
+
+# load json data
+set specfile [open $specfile_path r]
+set specdata_json [read $specfile]
+close $specfile
+set specdata [::json::json2dict $specdata_json]
+
+# set up data for the template to use
+set out {
+    # Disable timing analysis for clock domain crossing dedicated modules
+    set_false_path -through [get_pins -filter {NAME =~ *SyncAsync*/oSyncStages_reg[*]/D} -hier]
+    set_false_path -through [get_pins -filter {NAME =~ *SyncAsync*/oSyncStages*/PRE || NAME =~ *SyncAsync*/oSyncStages*/CLR} -hier]
+
+    set_false_path -through [get_pins -filter {NAME =~ *InstHandshake*/*/CLR} -hier]
+    set_false_path -from [get_cells -hier -filter {NAME =~ *InstHandshake*/iData_int_reg[*]}] -to [get_cells -hier -filter {NAME=~ *InstHandshake*/oData_reg[*]}]
+    
+    # are async_regs needed?
+}
+
+set f [open $outfile_path w]
+puts $f $out
+close $f
