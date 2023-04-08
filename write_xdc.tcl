b793ee49b9b91fbae9fbcd8c685bdd8daa870e7b
ArtVVB <arthur.brown@digilent.com>
2023-04-07 18:06:30 -0700

    Check if only one clock is present before specifying false_path constraints

--- write_xdc.tcl
+++ write_xdc.tcl
@@ -14,21 +14,26 @@ set specdata_json [read $specfile]
 close $specfile
 set specdata [::json::json2dict $specdata_json]
 
-set ip_name [dict get $specdata ip_name]
-set outfile_path ${script_dir}/intermediates/${ip_name}/${ip_name}_cdc.xdc
+    set ip_name [dict get $specdata ip_name]
+    set outfile_path ${script_dir}/intermediates/${ip_name}/${ip_name}_cdc.xdc
 
-# set up data for the template to use
-set out {
-    # Disable timing analysis for clock domain crossing dedicated modules
-    set_false_path -through [get_pins -filter {NAME =~ *SyncAsync*/oSyncStages_reg[*]/D} -hier]
-    set_false_path -through [get_pins -filter {NAME =~ *SyncAsync*/oSyncStages*/PRE || NAME =~ *SyncAsync*/oSyncStages*/CLR} -hier]
+    if {[llength [dict get $specdata clocks]] > 1} {
+        # set up data for the template to use
+        set out {
+            # Disable timing analysis for clock domain crossing dedicated modules
+            set_false_path -through [get_pins -filter {NAME =~ *SyncAsync*/oSyncStages_reg[*]/D} -hier]
+            set_false_path -through [get_pins -filter {NAME =~ *SyncAsync*/oSyncStages*/PRE || NAME =~ *SyncAsync*/oSyncStages*/CLR} -hier]
 
-    set_false_path -through [get_pins -filter {NAME =~ *InstHandshake*/*/CLR} -hier]
-    set_false_path -from [get_cells -hier -filter {NAME =~ *InstHandshake*/iData_int_reg[*]}] -to [get_cells -hier -filter {NAME=~ *InstHandshake*/oData_reg[*]}]
-    
-    # are async_regs needed?
-}
+            set_false_path -through [get_pins -filter {NAME =~ *InstHandshake*/*/CLR} -hier]
+            set_false_path -from [get_cells -hier -filter {NAME =~ *InstHandshake*/iData_int_reg[*]}] -to [get_cells -hier -filter {NAME=~ *InstHandshake*/oData_reg[*]}]
+            
+            # are async_regs needed?
+        }
+    } else {
+        # Empty constraint file if there are no CDCs
+        set out {}
+    }
 
-set f [open $outfile_path w]
-puts $f $out
-close $f
+    set f [open $outfile_path w]
+    puts $f $out
+    close $f
